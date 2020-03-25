e99dd40d93cedeeb3e6544781dfcaf8865abbae2
James Rogers <jarogers095@gmail.com>
2020-03-25 05:07:57 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,20 @@
+class Dog
+  attr_reader(:id)
+  attr_accessor(:name, :breed)
+  
+  def initialize(id: nil, name:, breed:)
+    @id = id
+    @name = name
+    @breed = breed
+  end
+  
+  def self.create_table()
+    sql = "CREATE TABLE IF NOT EXISTS dogs (id INTEGER PRIMARY KEY, name TEXT, breed TEXT);"
+    DB[:conn].execute(sql);
+  end
+  
+  
+  
+  
+  
+end
