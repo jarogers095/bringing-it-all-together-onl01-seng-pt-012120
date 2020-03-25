4306924e79f535036db571955005bfe639813718
James Rogers <jarogers095@gmail.com>
2020-03-25 05:34:06 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -9,7 +9,7 @@ class Dog
   end
   
   def self.create(attributes)
-    
+    new_dog = Dog.new()
   
   def self.create_table()
     sql = "CREATE TABLE IF NOT EXISTS dogs (id INTEGER PRIMARY KEY, name TEXT, breed TEXT);"
