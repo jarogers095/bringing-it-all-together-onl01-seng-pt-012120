62d53abc8dc058ad0218b3830d11a76fdc69b5fc
James Rogers <jarogers095@gmail.com>
2020-03-25 05:13:23 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -18,6 +18,16 @@ class Dog
     DB[:conn].execute(sql);
   end
   
+  def self.new_from_db(row)
+    new_dog = Dog.new(id: row[0], name: row[1], breed: row[2])
+    return new_dog
+  end
+  
+  def self.find_by_name(name)
+    sql = "CREATE TABLE IF NOT EXISTS dogs (id INTEGER PRIMARY KEY, name TEXT, breed TEXT);"
+    DB[:conn].execute(sql);
+  end
+  
   
   
 end
